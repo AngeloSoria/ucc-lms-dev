473f0d640cbc90fb6d3dd9c6eac4cae5f7eeec3a
Gelo_ <imangelosoria@gmail.com>
2024-10-24 19:02:46 +0800

    directory changed

--- src/views/partials/footer.php
+++
@@ -1,8 +0,0 @@
-<footer
-    class="text-white text-center py-4 bg-dark border-top border-5 border-success">
-    <div class="container">
-        <p class="mb-0">© 2024 Unida Christian College - Cavite | Learning Management System</p>
-    </div>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
