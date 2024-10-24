f1894c8d266acdf7b5a4b2a9a32be7a9afdecf8d
Gelo_ <imangelosoria@gmail.com>
2024-10-24 18:47:15 +0800

    fixes .env value retrieval

--- src/config/connection.php
+++ src/config/connection.php
@@ -14,11 +14,26 @@ class Database
         $dotenv = Dotenv::createImmutable(BASE_PATH);
         $dotenv->load();
 
-        $host = getenv('DB_HOST') or die('Missing DB_HOST environment variable') ;
-        $db   = getenv('DB_NAME') or die('Missing DB_NAME environment variable');
-        $user = getenv('DB_USER') or die('Missing DB_USER environment variable') ;
-        $pass = getenv('DB_PASS') or die('Missing DB_PASSWORD environment variable');
-        $port = getenv('DB_PORT') or die('Missing DB_PORT environment variable');
+        # Check if the connection is in localhost or production environment.
+        switch ($_ENV['APP_ENV']) {
+            case "local":
+                $host = "localhost";
+                $db   = "ucc_db";
+                $user = "root";
+                $pass = "";
+                $port = 3306;
+                break;
+            case "prod":
+                $host = $_ENV['DB_HOST'] or die('Missing DB_HOST environment variable');
+                $db   = $_ENV['DB_NAME'] or die('Missing DB_NAME environment variable');
+                $user = $_ENV['DB_USER'] or die('Missing DB_USER environment variable') ;
+                $pass = $_ENV['DB_PASS'] or die('Missing DB_PASSWORD environment variable');
+                $port = $_ENV['DB_PORT'] or die('Missing DB_PORT environment variable');
+                break;
+            default:
+                echo 'Unknown APP_ENV value. Please set it to either "local" or "prod".';
+                break;
+        }
 
         $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
         try {
